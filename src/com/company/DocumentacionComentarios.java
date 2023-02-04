4835fb288dfdd90fe4ff4b736fd412e00e58ab5b
rafopm <rafopm@gmail.com>
2023-02-03 20:38:20 -0500

    Comentarios y documentaciòn

---
+++ src/com/company/DocumentacionComentarios.java
@@ -0,0 +1,23 @@
+package com.company;
+
+public class DocumentacionComentarios {
+
+    /**
+     * Funcion principal punto de entreda a la apliaciòn
+     * @param args argumentos a recibir
+     * @since 1.0
+     */
+    public static void main(String[] args) {
+        /* Aquí un texto de comentario */
+
+        /*
+        1. Abrir fichero
+        2. Leer fichero
+        3. Procesar fichero
+        4. Cerrar fichero
+         */
+
+        // Imprimir texto
+        System.out.println("Hola Mundo"); // imprimir fichero
+    }
+}
---
+++ src/com/company/VariablesTiposDatos.java
@@ -0,0 +1,28 @@
+package com.company;
+
+public class VariablesTiposDatos {
+    public static void main(String[] args) {
+        //Literales de enteros
+        byte variableByte = 12;
+        short variableShort = 12;
+        int variableInt = 12;
+        long variableLong = 12;
+
+        int variableBinaria = 011010;
+        int variableHexadecimal = 0x1a;
+
+        //Literales de decimales
+        float variableFloat = 12.2F;
+        double variableDouble = 12.2D;
+
+        //Literales de caracteres y cadenas
+        String pais = "Espau00F1a";
+        char variableChar = 'a';
+        String variableString = "cadena";
+
+        //Literales subrayados
+        long tarjetaCredito = 1234_5678_9012_3456L;
+        long mascaraBinaria = 0b11010010_01101001_10010100_10010010;
+    }
+
+}
