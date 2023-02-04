14ce634d011019037e190c892eb0857692c00aaf
rafopm <rafopm@gmail.com>
2023-02-03 20:48:55 -0500

    Variables y tipos de datos

---
+++ src/com/company/Operadores.java
@@ -0,0 +1,54 @@
+package com.company;
+
+public class Operadores {
+
+    public static void main(String[] args) {
+        //Operadores
+
+        //aritméticos
+
+        // + - / * %
+        int number1 = 1;
+        int number2 = 2;
+
+        int resultado1 = number1 + number2;
+        int resultado2 = number1 - number2;
+        int resultado3 = number1 * number2;
+        int resultado4 = number1 / number2;
+
+        // logicos, relacion, comparacion, booleanos
+
+        /*
+        >
+        >=
+        <
+        <=
+        ==
+        !=
+        && and
+        || or
+        !
+
+         */
+
+        //Asignaciòn
+        /*
+        =
+        +=
+        -=
+        /=
+        *=
+        %=
+
+         */
+
+        //Incremento
+        // ++
+
+        //Decremento
+        // --
+
+        //Concatenación
+        // +
+    }
+}
