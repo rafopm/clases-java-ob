5f6f2fa21e4d5147d28dc05ef7a2b12622bb70be
rafopm <rafopm@gmail.com>
2023-02-06 20:27:27 -0500

    invocar funciones

---
+++ src/com/company/Funciones.java
@@ -0,0 +1,13 @@
+package com.company;
+
+public class Funciones {
+    public static void main(String[] args) {
+        holamundo();
+        holamundo();
+    }
+
+    public static void holamundo() {
+        System.out.println("Hola Mundo");
+        System.out.println("Hola Mundo");
+    }
+}
---
+++ src/com/otracompany/invocador.java
@@ -0,0 +1,10 @@
+package com.otracompany;
+
+import com.company.Funciones;
+
+public class invocador {
+    public static void main(String[] args) {
+        Funciones.holamundo();
+    }
+}
+
